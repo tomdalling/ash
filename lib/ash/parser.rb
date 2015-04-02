75862ac5a2ff8b54d42a27df7f94d8bd7a5ced2f
Tom Dalling <tom@tomdalling.com>
2015-04-03 01:06:22 +1100

    Added adjuncts

--- lib/ash/parser.rb
+++ lib/ash/parser.rb
@@ -9,7 +9,7 @@ module Ash
     rule(:ko) { str('ko') >> ws? }
     rule(:kun) { str('kun') >> ws? }
 
-    rule(:word) { match('[a-z]').repeat(1) >> ws? }
+    rule(:word) { match('[a-z\-]').repeat(1) >> ws? }
     rule(:verb) { str('na').maybe >> str('v') >> word }
     rule(:noun) { str('s') >> word }
     rule(:adjective) { str('l') >> word }
@@ -18,15 +18,22 @@ module Ash
     rule(:possessor) { ko >> noun1.as(:possessor) }
     rule(:noun_phrase) { noun1.as(:head) >> possessor.repeat.as(:possessors).maybe }
 
+    rule(:temporal_adjunct) { (str('timet') | str('timem') | str('time')) >> ws? >> noun_phrase }
+    rule(:instrumental_adjunct) { str('tem') >> str('na').maybe >> ws? >> noun_phrase }
+    rule(:causal_adjunct) { (str('tozem') | str('tozet')) >> ws? >> clause }
+    rule(:adjunct) { temporal_adjunct | instrumental_adjunct | causal_adjunct }
+
     rule(:verb_phrase) {
-      verb.as(:verb) >> (noun_phrase.as(:object) >> (kun >> noun_phrase.as(:indirect_object)).maybe).maybe
+      verb.as(:verb) >>
+      (noun_phrase.as(:object) >> (kun >> noun_phrase.as(:indirect_object)).maybe).maybe >>
+      adjunct.repeat.as(:adjuncts)
     }
 
     rule(:clause) { noun_phrase.as(:subject) >> verb_phrase.as(:verb_phrase) }
 
     rule(:statement) { clause.as(:statement) }
     rule(:command) { verb_phrase.as(:command) }
-    rule(:question) { ka >> statement.as(:question) }
+    rule(:question) { ka >> clause.as(:question) }
     rule(:sentence) { statement | command | question }
 
     root(:sentence)
--- spec/ash_spec.rb
+++ spec/ash_spec.rb
@@ -1,12 +1,8 @@
 require 'ash'
 require 'pp'
+require 'parslet/rig/rspec'
 
 RSpec.describe Ash::Parser do
-  it 'transforms the raw AST' do
-    result = Ash::Parser.new.send(:transform_ast, {a: [" 1 "]})
-    expect(result).to eq({a: ["1"]})
-  end
-
   it 'parses' do
     strs = [
       # I know
@@ -26,14 +22,46 @@ RSpec.describe Ash::Parser do
       # Will you eat food?
       # lit: ? you will-eat food
       'ka shae vesem ses',
+      # I will know you before tomorrow
+      'shi venem shae timet sha-tomorrow',
+      # I drank your coffee because I wanted it
+      # lit: I drank coffee of you because I wanted it
+      'shi veket shacafe ko shae tozet shi volet shel',
     ]
-    strs.each do |s|
-      puts s
-      result = Ash.parse(s)
-      expect(result).not_to be_nil
-      result
-    end
+    begin
+      strs.each do |s|
+        puts s
+        result = Ash.parse(s)
+        expect(result).not_to be_nil
+        result
+      end
 
-    pp Ash.parse(strs.last)
+      pp Ash.parse(strs.last)
+    rescue Parslet::ParseFailed => error
+      puts error.cause.ascii_tree
+    end
+  end
+
+  def should_parse(rule, strs)
+    parser = Ash::Parser.new.send(rule)
+    strs.each do |s|
+      expect(parser).to parse(s)
+    end
+  end
+
+  it 'parses adjuncts' do
+    should_parse(:temporal_adjunct, [
+      'time sha-tomorrow', # at/on tomorrow
+      'timet sha-tomorrow', # before tomorrow
+      'timem sha-tomorrow', # after tomorrow
+    ])
+    should_parse(:instrumental_adjunct, [
+      'tem shi', # with me
+      'temna shi', # without me
+    ])
+    should_parse(:causal_adjunct, [
+      'tozem shi volet shel', # because I wanted it
+      'tozet shi vapem', # so that I will sleep
+    ])
   end
 end
